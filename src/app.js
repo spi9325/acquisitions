8e95844bf3e93f631cfd5574f7fba370eb189d40
spi9325 <sanketinamdar72@gmail.com>
2025-09-12 20:52:09 +0530

    add helmet.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 //  this is for your setup express application
 import express from 'express';
 import logger from './config/logger';
+import helmet from 'helmet';
 
 const app = express();
-
+app.use(helmet());
 app.get('/', (req, res) => {
   logger.info('hello from Acqisitions');
   res.status(200).send('hello from acquisations');

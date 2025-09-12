2a88ee787694efa24a365249cf42a1dee8e9cb16
spi9325 <sanketinamdar72@gmail.com>
2025-09-12 20:03:21 +0530

    some auto formatted code and linted

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 //  this is for your setup express application
-import express from 'express'
+import express from 'express';
 
 const app = express();
 
-
-app.get('/', (req, res)=>{
-    res.status(200).send('hello from acquisations')
-})
+app.get('/', (req, res) => {
+  res.status(200).send('hello from acquisations');
+});
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
 //  this file is for managing like server is running correct
-import app from "./app.js"
+import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
 
-
-app.listen(PORT,()=>{
-    console.log(`listing on http://localhost: ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`listing on http://localhost: ${PORT}`);
+});
